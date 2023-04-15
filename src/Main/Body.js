3c2a0851836016af956afcff12f3e353d9bde5ed
Labesh <labheshwar.work@gmail.com>
2023-04-15 22:34:39 +0500

    Included Search Functionality

--- src/Main/Body.js
+++ src/Main/Body.js
@@ -1,13 +1,33 @@
-import Card from "./Card";
+import { useState } from 'react';
+import Card from './Card';
 import data from '../restaurantData.json';
 
 const Body = () => {
+  const [searchText, setSearchText] = useState('');
+  const [restaurantData, setRestaurantData] = useState(data);
+
+  const filterRestaurant = () => {
+    const filteredData = data.filter(item => item.data.name.toLowerCase().includes(searchText.toLowerCase()))  
+    setRestaurantData(filteredData);
+  }
+  
   return (
-    <main>
-      {data.map((item) => {
-        return <Card key={item.data.uuid} {...item.data} />;
-      })}
-    </main>
+    <>
+      <div className='search'>
+        <input
+          type='search'
+          value={searchText}
+          placeholder='Search for restaurants'
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button onClick={filterRestaurant}>Search</button>
+      </div>
+      <main>
+        {restaurantData.map((item) => {
+          return <Card key={item.data.uuid} {...item.data} />;
+        })}
+      </main>
+    </>
   );
 };
 
