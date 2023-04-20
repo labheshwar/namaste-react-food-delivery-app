c1d434e8604a2dee3576dbed581b265bb1c03abf
Labesh <labheshwar.work@gmail.com>
2023-04-20 13:23:52 +0500

    Getting Data from API

--- src/Main/Body.js
+++ src/Main/Body.js
@@ -1,17 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from './Card';
 import data from '../restaurantData.json';
 import Search from './Search';
+import { API_URL } from '../config';
 
 const Body = () => {
   const [searchText, setSearchText] = useState('');
-  const [restaurantData, setRestaurantData] = useState(data);
+  const [restaurantData, setRestaurantData] = useState([]);
 
   const filterRestaurant = (value) => {
     const filteredData = data.filter((item) =>
       item.data.name.toLowerCase().includes(value.toLowerCase())
     );
     setRestaurantData(filteredData);
+  };
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  const callApi = async () => {
+    const data = await fetch(API_URL);
+    const json = await data.json();
+    const destructuredData = json?.data?.cards[2]?.data?.data?.cards;
+    console.log(destructuredData);
+    setRestaurantData(destructuredData);
   };
 
   return (
