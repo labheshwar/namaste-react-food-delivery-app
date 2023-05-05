dff4cc2dd565d4cd0470b1909620183c6937a978
Labesh <labheshwar.work@gmail.com>
2023-05-05 17:08:09 +0500

    Navbar CSS Complete

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-    <div className='logo'>
+    <div className='mx-auto md:flex md:items-center md:mx-0'>
       <img
+        className='h-14 w-14 my-3'
         src='https://images.vexels.com/media/users/3/136309/isolated/preview/c6539229ad5c57c313d95711a1e676db-logo-burger-fast-food-by-vexels.png'
         alt='Logo'
       />
-      <h1>
-        <a href='/'>Food Anomaly</a>
+      <h1 className='font-bold text-xl hidden md:block md:ml-4 lg:ml-5'>
+        <Link to='/'>Food Anamoly</Link>
       </h1>
     </div>
   );
@@ -17,20 +18,40 @@ const Logo = () => {
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>
+    <nav className='flex flex-col justify-between px-5 md:flex-row md:items-center bg-teal-800 text-white'>
       <Logo />
-      <ul>
+      <ul className='flex justify-between mb-7  md:text-left md:[&>*]:mr-4 md:mb-0 lg:[&>*]:mr-5'>
         <li>
-          <Link to='/'>Home</Link>
+          <Link
+            to='/'
+            className='font-semibold transition duration-150 ease-in rounded border-b-2 p-2 border-transparent hover:border-teal-100 hover:text-teal-100'
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to='/about'>About</Link>
+          <Link
+            to='/about'
+            className='font-semibold transition duration-150 ease-in rounded border-b-2 p-2 border-transparent hover:border-teal-100 hover:text-teal-100'
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link to='/contact'>Contact</Link>
+          <Link
+            to='/contact'
+            className='font-semibold transition duration-150 ease-in rounded border-b-2 p-2 border-transparent hover:border-teal-100 hover:text-teal-100'
+          >
+            Contact
+          </Link>
         </li>
         <li>
-          <Link to='/login'>Login</Link>
+          <Link
+            to='/login'
+            className='font-semibold transition duration-150 ease-in rounded border-b-2 p-2 border-transparent hover:border-teal-100 hover:text-teal-100'
+          >
+            Login
+          </Link>
         </li>
       </ul>
     </nav>
